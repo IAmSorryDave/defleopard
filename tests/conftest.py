115e3cde8911c4b03d12750dcccfb6268278e073
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-08-08 16:16:05 -0700

    Add MyClass and MyDataClass with methods

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,38 @@
 import pytest
+from dataclasses import dataclass
 
+def my_test_function(*args, c=2, d="baz", z=False, **kwargs):
+    return locals().copy()
+
+
+class MyClass:
+    z = True
+
+    def my_bare_method(*args, c=2, d="baz", z=False, **kwargs):
+        return my_function(*args, c=c, d=d, z=z, **kwargs)
+
+    my_class_method = classmethod(my_function)
+
+    my_method = my_function
+
+    my_static_method = staticmethod(my_function)
+
+    my_property = property(my_function)
+
+
+class MySubClass(MyClass):
+    d = "qux"
+
+
+@dataclass
+class MyDataClass:
+    c: int = 3
+    d: str = "baz"
+    z: bool = True
+
+    my_method = my_function
+
+    my_property = property(my_function)
 
 @pytest.fixture
 def project_directory_path():
